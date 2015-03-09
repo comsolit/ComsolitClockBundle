06af05f9e7a79530e47709639bd34d76b41941b3
Christian Lecherbauer <lecherbauer@comsolit.ch>
2015-03-09 16:16:24 +0100

    add minimal Symfony as a fixture

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once __DIR__ . '/../Clock.php';
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+if (!is_file($loaderFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+}
+$loader = require $loaderFile;
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
